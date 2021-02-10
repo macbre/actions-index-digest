2d1b4364cbeeaeddb769732dd343d287dab8e393
Maciej Brencz <maciej.brencz@gmail.com>
2021-02-10 16:25:10 +0100

    Add the_table

--- sql/0000_setup.sql
+++ sql/0000_setup.sql
@@ -4,3 +4,9 @@ CREATE USER 'digest'@'%' IDENTIFIED WITH mysql_native_password BY 's3cr3t';
 
 GRANT ALL ON index_digest.* TO 'digest'@'%';
 
+-- the first table (with no issues)
+CREATE TABLE `the_table` (
+	`item_id` int(9) NOT NULL AUTO_INCREMENT,
+	`foo` varbinary(16) NOT NULL DEFAULT '',
+	PRIMARY KEY (`item_id`)
+);
